a9293ca4fc545b3cb0f414fafb373872c1d84645
PeterisVL <147054771+PeterisVL@users.noreply.github.com>
2026-09-23 10:00:04 +0000

    test

--- src/lab2_1.c
+++ src/lab2_1.c
@@ -12,35 +12,32 @@
 */
 
 int sum_to_n(int n) {
-    // TODO: implement sum with a for loop
-    int sum = 0;
-    int val;
-    
-    for (int i = 1; i <= n; i++)
-    {
-        printf("Enter the %d position number: ", i);
-        scanf("%d", &val);
-        sum = sum + val;
-    }
-    return sum; // placeholder
+  // TODO: implement sum with a for loop
+  int sum = 0;
+  int val;
+
+  for (int i = 1; i <= n; i++) {
+    printf("Enter the %d position number: ", i);
+    scanf("%d", &val);
+    sum = sum + val;
+  }
+  return sum;  // placeholder
 }
 
 int main(void) {
-    int n;
+  int n;
 
-    printf("Enter a positive integer n: ");
+  printf("Enter a positive integer n: ");
+  scanf("%d", &n);
+
+  while (n < 1) {
+    printf("N must be more or equal to 1: ");
     scanf("%d", &n);
+  }
 
-    while (n < 1) {
-        printf("Error: n must be a positive integer.\n Input a positive integer n: ");
-        scanf("%d", &n);
-    }
-    int result = sum_to_n(n);
+  int res = sum_to_n(n);
+  // TODO: validate input, call function, and print result
+  printf("Result is %d\n", res);
 
-    printf("The sum result is: ");
-    printf("%d", result);
-    printf("\n");
-    // TODO: validate input, call function, and print result
-
-    return 0;
+  return 0;
 }
